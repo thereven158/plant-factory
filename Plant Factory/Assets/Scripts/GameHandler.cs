172f568faa1e1f05ce3f51a8c03f48baa7dfab65
Muhammad Yoga Nugraha Putra <myoga82@rocketmail.com>
2019-02-19 16:23:12 +0700

    Adding a limit for vision, button to inc/red the vision, using a Tilemaps
    need to tidy up some code

--- Plant Factory/Assets/Scripts/GameHandler.cs
+++ Plant Factory/Assets/Scripts/GameHandler.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Tilemaps;
 
 public class GameHandler : MonoBehaviour
 {
+    //Reduce HP/sec
     public float reduceHpRate = 1f;
     float nextReduceHp = 0.0f;
 
@@ -11,12 +13,31 @@ public class GameHandler : MonoBehaviour
     private HealthSystem healthSystem = new HealthSystem(100);
     private int currentHp = 100;
 
+    //lighting tilemap
+    public Tilemap DarkMap;
+    public Tilemap BlurredMap;
+    public Tilemap Background;
+    public Tile DarkTile;
+    public Tile BlurredTile;
+
     // Start is called before the first frame update
     void Start()
     {
-        
-
         healthBar.Setup(healthSystem);
+
+        //lighting tilemap
+        DarkMap.origin = BlurredMap.origin = Background.origin;
+        DarkMap.size = BlurredMap.size = Background.size;
+
+        foreach (Vector3Int p in DarkMap.cellBounds.allPositionsWithin)
+        {
+            DarkMap.SetTile(p, DarkTile);
+        }
+
+        foreach (Vector3Int p in BlurredMap.cellBounds.allPositionsWithin)
+        {
+            BlurredMap.SetTile(p, BlurredTile);
+        }
     }
 
     // Update is called once per frame
---
+++ Plant Factory/Assets/Scripts/SMask.cs
@@ -0,0 +1,59 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SMask : MonoBehaviour
+{
+
+    [Range(0.05f, 0.2f)]
+    public float flickTime;
+
+    [Range(0.02f, 0.09f)]
+    public float addSize;
+
+    public Transform target;
+
+    private float timer;
+    private bool bigger;
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+
+        if(timer > flickTime)
+        {
+            if(bigger)
+            {
+                transform.localScale = new Vector3(transform.localScale.x + addSize,
+                                           transform.localScale.y + addSize,
+                                           transform.localScale.z);
+            }
+            else
+            {
+                transform.localScale = new Vector3(transform.localScale.x - addSize,
+                                           transform.localScale.y - addSize,
+                                           transform.localScale.z);
+            }
+
+            timer = 0;
+            bigger = !bigger;
+        }
+
+        transform.position = Vector3.MoveTowards(transform.position, target.position, 20 * Time.deltaTime);
+    }
+
+    public void AddVision ()
+    {
+        transform.localScale = new Vector3(transform.localScale.x + 0.1f,
+                                           transform.localScale.y + 0.1f,
+                                           transform.localScale.z);
+    }
+
+    public void ReduceVision()
+    {
+        transform.localScale = new Vector3(transform.localScale.x - 0.1f,
+                                           transform.localScale.y - 0.1f,
+                                           transform.localScale.z);
+    }
+}
