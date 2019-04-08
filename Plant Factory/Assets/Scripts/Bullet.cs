a4d9c66879cda86c6d18520b1b0f2b95fe73619c
thereven158 <myoga82@rocketmail.com>
2019-04-09 06:28:04 +0700

    Add Player Shoot, fix win button ui

---
+++ Plant Factory/Assets/Scripts/Bullet.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed = 20f;
+    public Rigidbody2D rB;
+    GameObject enemy;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rB.velocity = transform.right * speed;
+        enemy = GameObject.FindGameObjectWithTag("Enemy");
+    }
+
+    private void OnTriggerEnter2D(Collider2D enemyTag)
+    {
+        
+        if(enemyTag.tag == "Enemy")
+        {
+            Destroy(gameObject);
+            Destroy(enemy);
+        }
+    }
+}
